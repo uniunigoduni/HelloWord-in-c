6a69408cb838170eb22ff52b1209b544d47f371b
doginu <150897841+uniunigoduni@users.noreply.github.com>
2023-12-03 02:29:36 +0900

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define 🥩 int
+#define 🤟 main
+#define 😂 ()
+#define 👍 {
+#define 🍖 printf
+#define 😋 ("Hello, World!\n");
+#define 👉 return
+#define 😁 0;
+#define 👈 }
+
+🥩 🤟 😂 👍 🍖 😋 👉 😁 👈
